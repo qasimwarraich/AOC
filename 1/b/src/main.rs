3e0e15a716c0c8a082efc69afa073b30e64c8895
qasimwarraich <qasim.warraich@gmail.com>
2022-12-01 14:23:15 +0100

    day1(b): Refactor solution a little bit

--- 1/b/src/main.rs
+++ 1/b/src/main.rs
@@ -1,9 +1,10 @@
 use std::{
     fs::File,
-    io::{self, BufRead, BufReader},
+    error::Error,
+    io::{BufRead, BufReader},
 };
 
-fn main() -> Result<(), io::Error> {
+fn main() -> Result<(), Box<dyn Error>> {
     let filename = "./input.txt";
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
@@ -18,16 +19,15 @@ fn main() -> Result<(), io::Error> {
             elves.push(counter);
             counter = 0;
         } else {
-            let count: i32 = line.parse().unwrap();
+            let count: i32 = line.parse()?;
             counter += count;
         }
     }
 
     elves.sort();
-
     let top3 = &elves[elves.len() - 3..];
     let sum: i32 = top3.iter().sum();
-    println!("Top3 Sum: {:?}", sum);
 
+    println!("Top3 Sum: {:?}", sum);
     Ok(())
 }
