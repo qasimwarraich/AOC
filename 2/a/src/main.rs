95c275d8f9c544a359b640885d2ac8d72c282e67
qasimwarraich <qasim.warraich@gmail.com>
2022-12-02 18:56:38 +0100

    day2: Upload solutions

---
+++ 2/a/src/main.rs
@@ -0,0 +1,34 @@
+use std::{
+    collections::HashMap,
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let filename = "./input.txt";
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    let game_code = HashMap::from([("X", 1), ("Y", 2), ("Z", 3), ("A", 1), ("B", 2), ("C", 3)]);
+    let moves = Vec::from([1, 2, 3]);
+    let mut total_score = 0;
+
+    let lines = reader.lines().enumerate();
+    for (_index, line) in lines {
+        let line = line?;
+        let char_vec = &line.split(" ").collect::<Vec<&str>>();
+        let p1 = game_code.get(char_vec[0]).unwrap();
+        let p2 = game_code.get(char_vec[1]).unwrap();
+
+        if p1 == p2 {
+            total_score += 3 + p2;
+        } else if moves[p1 % 3] == *p2 {
+            total_score += 6 + p2;
+        } else {
+            total_score += p2;
+        }
+    }
+    println!("{}", total_score);
+    Ok(())
+}
---
+++ 2/b/src/main.rs
@@ -0,0 +1,34 @@
+use std::{
+    collections::HashMap,
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let filename = "./input.txt";
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    let game_code = HashMap::from([("X", 1), ("Y", 2), ("Z", 3), ("A", 1), ("B", 2), ("C", 3)]);
+    let moves = Vec::from([1, 2, 3]);
+    let mut total_score = 0;
+
+    let lines = reader.lines().enumerate();
+    for (_index, line) in lines {
+        let line = line?;
+        let char_vec = &line.split(" ").collect::<Vec<&str>>();
+        let p1 = game_code.get(char_vec[0]).unwrap();
+        let p2 = game_code.get(char_vec[1]).unwrap();
+
+        if *p2 == 2 {
+            total_score += 3 + p1;
+        } else if *p2 == 3 {
+            total_score += moves[p1 % 3] + 6;
+        } else {
+            total_score += moves[(p1 + 1)  % 3];
+        }
+    }
+    println!("{}", total_score);
+    Ok(())
+}
