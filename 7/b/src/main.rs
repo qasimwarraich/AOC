44e552545ebe4db1f9357ba79d73c43967b77cac
qasimwarraich <qasim.warraich@gmail.com>
2022-12-08 18:14:08 +0100

    day7: Add solutions for day 7
    These solutions need to be refactored!

---
+++ 7/b/src/main.rs
@@ -0,0 +1,72 @@
+use std::{collections::HashMap, error::Error};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let total_space = 70_000_000;
+    let required_space = 3_000_0000;
+
+    let file = include_str!("./input.txt");
+
+    let mut log_vec: Vec<String> = file.trim_end().split("$").map(|x| x.to_string()).collect();
+
+    log_vec = log_vec
+        .iter()
+        .map(|x| x.replace("\n", " "))
+        .map(|x| x.trim().to_string())
+        .filter(|x| x != "")
+        .collect();
+
+    let mut file_tree: Vec<String> = Vec::new();
+    let mut dir_sizes: HashMap<String, usize> = HashMap::new();
+    let mut cwd: String;
+
+    file_tree.push("/".to_string());
+
+    for entry in log_vec {
+        if entry.starts_with("cd") {
+            let c: Vec<_> = entry.split(" ").collect();
+            if c[1] == ".." {
+                file_tree.pop();
+                continue;
+            }
+            cwd = c[1].to_string();
+            if file_tree.last().unwrap() != &cwd {
+                file_tree.push(cwd.clone());
+            }
+        }
+
+        let file_sizes: Vec<usize> = entry
+            .split_whitespace()
+            .filter_map(|token| token.parse().ok())
+            .collect();
+
+        let mut path = "".to_string();
+        for file in &file_tree {
+            let dir_name = format!("{}{}", file, "/");
+            path += &dir_name;
+
+            for file_size in &file_sizes {
+                if let Some(count) = dir_sizes.get(&path) {
+                    dir_sizes.insert(path.clone(), count + file_size);
+                } else {
+                    dir_sizes.insert(path.clone(), *file_size);
+                }
+            }
+        }
+    }
+
+    let used_space = dir_sizes.get("//").unwrap();
+    let free_space = total_space - used_space;
+    let needed_for_update = required_space - free_space;
+
+    let mut candidates = Vec::new();
+    for (_dir, size) in dir_sizes {
+        if size >= needed_for_update {
+            candidates.push(size);
+        }
+    }
+
+    candidates.sort();
+    println!("candidates = {:?}", candidates);
+
+    Ok(())
+}
